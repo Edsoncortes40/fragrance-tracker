84f8724de2a0ec80da29330add8f144d745bc8f2
Edsoncortes40 <saul_cortes00@yahoo.com>
2024-02-12 14:22:49 -0500

    adding firestore authentication via Google Popup

---
+++ web-client/app/firebase/firebase.ts
@@ -0,0 +1,47 @@
+// Import the functions you need from the SDKs you need
+import {initializeApp} from "firebase/app";
+import {
+    getAuth, 
+    signInWithPopup,
+    GoogleAuthProvider,
+    onAuthStateChanged,
+    User} from "firebase/auth";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBgrt4Agp4vILbGRhidl2ocr7rQHrFCCzc",
+  authDomain: "fragrance-tracker.firebaseapp.com",
+  projectId: "fragrance-tracker",
+  appId: "1:1040977709758:web:7593356f0ea007888e2cef",
+  measurementId: "G-NTSJ6ZHTN6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+/**
+ * signs the user in with a Google Popup.
+ * @returns A promise that resolves with the user's credentials.
+ */
+export function signInWithGoogle() {
+    return signInWithPopup(auth, new GoogleAuthProvider());
+}
+
+/**
+ * Signs the user out.
+ * @returns A promise that resolves when the user is signed out.
+ */
+export function signOut() {
+    return auth.signOut();
+}
+
+/**
+ * Trigger a callback when user auth state changes.
+ * @returns A function to unsubscribe callback.
+ */
+export function onAuthStateChangedHelper(callback: (user: User | null) => void) {
+    return onAuthStateChanged(auth, callback);
+}
---
+++ web-client/app/navbar/navbar.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import styles from './navbar.module.css';
+import Image from 'next/image';
+import Link from 'next/link';
+import SignIn from './sign-in';
+import {onAuthStateChangedHelper} from '../firebase/firebase';
+import {useEffect, useState} from 'react';
+import { User } from 'firebase/auth';
+
+
+export default function Navbar(){
+    // Init user State
+    const [user, setUser] = useState<User | null>(null);
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChangedHelper((user) => {
+            setUser(user);
+        });
+
+        // cleanup Subscription on unmount
+        return () => unsubscribe();
+    });
+
+    return (
+        <nav className={styles.nav}>
+            <Link href="/">
+                <Image width={60} height={60}
+                src="/fragLogo.jpeg" alt="frag logo"/>
+            </Link>
+            <h1>Fragrance Tracker</h1>
+            <SignIn user={user}/>
+        </nav>
+    )
+}
